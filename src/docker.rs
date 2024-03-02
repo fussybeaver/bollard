6c50594ca041b15c2dbf8c81d8cb08b1402b0afc
Niel Drummond <niel@drummond.lu>
2024-03-02 16:21:42 +0000

    feat: Bump the default API version to 1.44

--- src/docker.rs
+++ src/docker.rs
@@ -73,7 +73,7 @@ const DEFAULT_TIMEOUT: u64 = 120;
 /// Default Client Version to communicate with the server.
 pub const API_DEFAULT_VERSION: &ClientVersion = &ClientVersion {
     major_version: 1,
-    minor_version: 43,
+    minor_version: 44,
 };
 
 /// 2 years from ct_logs 0.9 release
