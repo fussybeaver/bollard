89f6462ef8c58c775f7f9817e1a7093b399cab44
Niel Drummond <niel@drummond.lu>
2024-12-11 07:04:40 +0000

    tests(swarm): Feature lock and circleci task for swarm test

--- tests/swarm_test.rs
+++ tests/swarm_test.rs
@@ -1,16 +1,10 @@
-#![type_length_limit = "2097152"]
-
-use bollard::errors::Error;
-use bollard::swarm::*;
-use bollard::Docker;
-
-use tokio::runtime::Runtime;
-
 #[macro_use]
 pub mod common;
-use crate::common::*;
 
-async fn swarm_test(docker: Docker) -> Result<(), Error> {
+#[cfg(feature = "test_swarm")]
+async fn swarm_test(docker: bollard::Docker) -> Result<(), bollard::errors::Error> {
+    use bollard::swarm::*;
+
     // init swarm
     let config = InitSwarmOptions {
         listen_addr: "0.0.0.0:2377",
@@ -38,7 +32,11 @@ async fn swarm_test(docker: Docker) -> Result<(), Error> {
     Ok(())
 }
 
+#[cfg(feature = "test_swarm")]
 #[test]
 fn integration_test_swarm() {
+    use crate::common::run_runtime;
+    use bollard::Docker;
+    use tokio::runtime::Runtime;
     connect_to_docker_and_run!(swarm_test);
 }
