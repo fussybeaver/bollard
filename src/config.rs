3b468126353c6d8aa363aef9da5073a5b739360f
benodiwal <s474996633@gmail.com>
2025-12-26 00:46:44 +0530

    feat: add config API with generated query parameters

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,5 @@
 //! Config API: manage and inspect docker configs within a swarm
+#![allow(deprecated)]
 
 pub use crate::models::*;
 
@@ -35,6 +36,10 @@ use std::{collections::HashMap, hash::Hash};
 /// let options: ListConfigsOptions<&str> = Default::default();
 /// ```
 #[derive(Debug, Clone, Default, PartialEq, Serialize)]
+#[deprecated(
+    since = "0.19.0",
+    note = "use the OpenAPI generated bollard::query_parameters::ListConfigsOptions and associated ListConfigsOptionsBuilder"
+)]
 pub struct ListConfigsOptions<T>
 where
     T: Into<String> + Eq + Hash + serde::ser::Serialize,
@@ -46,6 +51,23 @@ where
     ///  - `names`=`<name>` a multiple config's name comma separated
     #[serde(serialize_with = "crate::docker::serialize_as_json")]
     pub filters: HashMap<T, Vec<T>>,
+}
+
+impl<T> From<ListConfigsOptions<T>> for crate::query_parameters::ListConfigsOptions
+where
+    T: Into<String> + Eq + Hash + serde::ser::Serialize,
+{
+    fn from(opts: ListConfigsOptions<T>) -> Self {
+        crate::query_parameters::ListConfigsOptionsBuilder::default()
+            .filters(
+                &opts
+                    .filters
+                    .into_iter()
+                    .map(|(k, v)| (k.into(), v.into_iter().map(T::into).collect()))
+                    .collect(),
+            )
+            .build()
+    }
 }
 
 /// Parameters used in the [Update Config API](Docker::update_config())
@@ -61,9 +83,21 @@ where
 /// };
 /// ```
 #[derive(Debug, Copy, Clone, Default, PartialEq, Serialize)]
+#[deprecated(
+    since = "0.19.0",
+    note = "use the OpenAPI generated bollard::query_parameters::UpdateConfigOptions and associated UpdateConfigOptionsBuilder"
+)]
 pub struct UpdateConfigOptions {
     /// The version number of the config object being updated. This is required to avoid conflicting writes. This version number should be the value as currently set on the config before the update.
     pub version: u64,
+}
+
+impl From<UpdateConfigOptions> for crate::query_parameters::UpdateConfigOptions {
+    fn from(opts: UpdateConfigOptions) -> Self {
+        crate::query_parameters::UpdateConfigOptionsBuilder::default()
+            .version(opts.version as i64)
+            .build()
+    }
 }
 
 impl Docker {
@@ -101,19 +135,16 @@ impl Docker {
     ///
     /// docker.list_configs(options);
     /// ```
-    pub async fn list_configs<T>(
+    pub async fn list_configs(
         &self,
-        options: Option<ListConfigsOptions<T>>,
-    ) -> Result<Vec<Config>, Error>
-    where
-        T: Into<String> + Eq + Hash + serde::ser::Serialize,
-    {
+        options: Option<impl Into<crate::query_parameters::ListConfigsOptions>>,
+    ) -> Result<Vec<Config>, Error> {
         let url = "/configs";
 
         let req = self.build_request(
             url,
             Builder::new().method(Method::GET),
-            options,
+            options.map(Into::into),
             Ok(BodyType::Left(Full::new(Bytes::new()))),
         );
 
@@ -280,14 +311,14 @@ impl Docker {
         &self,
         config_id: &str,
         config_spec: ConfigSpec,
-        options: UpdateConfigOptions,
+        options: impl Into<crate::query_parameters::UpdateConfigOptions>,
     ) -> Result<(), Error> {
         let url = format!("/configs/{config_id}/update");
 
         let req = self.build_request(
             &url,
             Builder::new().method(Method::POST),
-            Some(options),
+            Some(options.into()),
             Docker::serialize_payload(Some(config_spec)),
         );
 
