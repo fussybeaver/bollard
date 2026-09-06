a85291d202ec1041d6e61988c36b12172b09b44b
Niel Drummond <niel@drummond.lu>
2026-09-06 12:25:39 +0100

    test(buildkit): close J1 coverage gaps

--- src/grpc/driver/mod.rs
+++ src/grpc/driver/mod.rs
@@ -1585,7 +1585,7 @@ mod tests {
             .entitlement(Entitlement::NetworkHost)
             .entitlement(Entitlement::SecurityInsecure)
             .build();
-        let (request, _) = build_definition_solve_request(
+        let (request, build_ref) = build_definition_solve_request(
             bollard_buildkit_proto::pb::Definition::default(),
             &DefinitionExporter::Local(PathBuf::from("/out")),
             &options,
@@ -1593,6 +1593,8 @@ mod tests {
             None,
         );
 
+        assert!(!build_ref.as_ref().is_empty());
+        assert_eq!(request.r#ref, build_ref.as_ref());
         assert_eq!(
             request.entitlements,
             vec![
